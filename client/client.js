e2859109d417d4a7d540ac8a6f7b35a157e6cfd8
Samuel Singleton <samuel.j.singleton@durham.ac.uk>
2025-01-19 15:40:16 +0000

    Allow users to add new players and their goals scored. This input is stored in a json file on the server side.

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 button = document.getElementById("p1")
 
-button.addEventListener('click', function(event){
+button.addEventListener('click', async function(event){
    fetch('http://127.0.0.1:8080/p1')
     .then(response => response.text())
     .then(body => document.getElementById('p1-tab').innerHTML=body)
@@ -8,3 +8,23 @@ button.addEventListener('click', function(event){
  });
 
 
+const form = document.getElementById("add-item");
+const modal = new bootstrap.Modal(document.getElementById('addItemModal'));
+form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const formJSON = JSON.stringify(Object.fromEntries(formData.entries()));
+    console.log("Form data", formJSON);
+    const response = await fetch("/api/player/add",{
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+          },
+        body: formJSON
+    })
+    if (response.ok){
+        modal.hide();
+        form.reset();
+    }
+    console.log("SKIB", response)
+})
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const app = express()
+const fs = require("fs")
+app.use(express.json());
 
 app.use(express.static('client'));
+const utils = require('./node_modules/utils');
 
-app.get("/p1", function(req,resp){
+
+app.post("/api/player/add", async function(req, resp){
+
+    let items = [];
+    if (fs.existsSync("./players.json")) {
+        let data = fs.readFileSync("./players.json", "utf8");
+        if (data) {
+            items = JSON.parse(data);
+        }
+    }
+
+    let itemName = req.body.name;
+    let id = items.length;
+
+    let newItem = {"id" : id, "name" : itemName, "goalsScored": req.body.goalsScored};
+    console.log("New Item Added:");
+    console.log(newItem);
+
+    items.push(newItem);
+    let itemsText = JSON.stringify(items);
+    fs.writeFileSync("./players.json", itemsText);
+
+    resp.sendStatus(200)
+})
+
+app.get("/p1", async function(req,resp){
     resp.send("Samuel Singleton")
 })
+
 
 app.listen(8080)
